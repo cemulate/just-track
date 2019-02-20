68ca29139496de573eb2c98ec03f834932cd295b
Chase Meadors <c.ed.mead@gmail.com>
2019-02-20 03:39:46 -0600

    Updates

--- precache-manifest.0e9169623492a682b37d75513abc9133.js
+++ precache-manifest.0e9169623492a682b37d75513abc9133.js
@@ -8,10 +8,6 @@ self.__precacheManifest = [
     "url": "main.js"
   },
   {
-    "revision": "4db0215c3d08ab2768aa79fa1eed7e94",
-    "url": "index.html"
-  },
-  {
     "revision": "939c936b0ca4b19d6d530cd6c6f45449",
     "url": "favicon.ico"
   }
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.47866ca9649942a58bde72234cb3a8b8.js"
+  "precache-manifest.0e9169623492a682b37d75513abc9133.js"
 );
 
 /**
