59fe0d0697e3108d91dda46f1e701b4842d65a90
Chase Meadors <c.ed.mead@gmail.com>
2019-02-20 03:43:49 -0600

    Updates

--- precache-manifest.47866ca9649942a58bde72234cb3a8b8.js
+++ precache-manifest.47866ca9649942a58bde72234cb3a8b8.js
@@ -8,6 +8,10 @@ self.__precacheManifest = [
     "url": "main.js"
   },
   {
+    "revision": "4db0215c3d08ab2768aa79fa1eed7e94",
+    "url": "index.html"
+  },
+  {
     "revision": "939c936b0ca4b19d6d530cd6c6f45449",
     "url": "favicon.ico"
   }
