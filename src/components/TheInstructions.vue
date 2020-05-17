28e29a79e6846b63ed6da400be632552e31c9fe4
Chase Meadors <c.ed.mead@gmail.com>
2020-05-16 19:43:07 -0600

    Tweak instructions

--- src/components/TheInstructions.vue
+++ src/components/TheInstructions.vue
@@ -13,9 +13,13 @@
             <ul>
                 <li>When recording, the tracker will implicitly default to the <strong>None</strong> task.</li>
                 <li>To switch to another task, add one in the task list and press the <strong>clock button</strong>, or its assigned <strong>hotkey</strong>.</li>
-                <li>To return to tracking the <strong>None</strong> task, press the <strong>spacebar</strong>.</li>
+                <li>To return to tracking the <strong>None</strong> task, press the <strong>spacebar</strong>. This task is meant to be used for breaks, lunch, or otherwise tracked but unfocused time.</li>
                 <li>To stop tracking entirely, click the tracker again.</li>
             </ul>
+            <p>
+                A time entry in the history is created when switching off the tracked task.
+                No time entries shorter than a minute are recorded.
+            </p>
         </div>
     </div>
 </article>
